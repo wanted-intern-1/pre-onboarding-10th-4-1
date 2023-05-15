2e249da41661c7c085868412e8ec44d42b690b5f
VictoryJu <kob0805@naver.com>
2023-05-16 03:20:44 +0900

    feat: search item hover,click 이벤트 발생시 문구 출력

--- src/components/search/SearchItem.tsx
+++ src/components/search/SearchItem.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { ITodo } from "../../types/common";
 import { styled } from "styled-components";
 import Highlighter from "react-highlight-words";
 import { theme } from "../../styles/theme";
+import useHover from "../../hooks/useHover";
 
 type Props = {
   todo: ITodo;
@@ -10,8 +11,15 @@ type Props = {
 };
 
 const SearchItem = ({ todo, inputText }: Props) => {
+  const [hoverRef, isHoverd] = useHover<HTMLLIElement>();
+  const [clickId, setClickId] = useState("");
+
+  useEffect(() => {
+    setClickId("");
+  }, [isHoverd]);
+
   return (
-    <S.TodoLine>
+    <S.TodoLine ref={hoverRef} onClick={() => setClickId(todo.id)}>
       <Highlighter
         highlightStyle={{
           color: theme.colors.green500,
@@ -22,6 +30,12 @@ const SearchItem = ({ todo, inputText }: Props) => {
       >
         {todo.title}
       </Highlighter>
+      {isHoverd && clickId !== todo.id ? (
+        <S.HoverNotice>hover</S.HoverNotice>
+      ) : (
+        ""
+      )}
+      {clickId === todo.id ? <S.ClickNotice>click</S.ClickNotice> : ""}
     </S.TodoLine>
   );
 };
@@ -34,6 +48,17 @@ const S = {
       background-color: ${({ theme }) => theme.colors.neutral100};
       border-radius: 3px;
     }
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  `,
+  HoverNotice: styled.div`
+    color: #3211ff;
+    font-size: 8px;
+  `,
+  ClickNotice: styled.div`
+    color: #ff112e;
+    font-size: 8px;
   `,
 };
 
