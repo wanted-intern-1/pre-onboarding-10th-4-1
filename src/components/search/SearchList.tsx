b217b6685ef092e8c2ddfbd434d0baf6d3375df1
Woohyun Park <woohyunpark.mail@gmail.com>
2023-05-17 23:13:52 +0900

    fix, useInfinityQuery 상태 세분화

--- src/components/search/SearchList.tsx
+++ src/components/search/SearchList.tsx
@@ -17,16 +17,16 @@ type Props = {
 const SearchList = ({ onSubmit, inputText, setIsSearchLoading }: Props) => {
   const ref = useRef<HTMLDivElement>(null);
   const debouncedInputText = useDebounce(inputText);
-  const [isFetching, data, fetchNextPage, hasNextPage] =
+  const { isLoading, isFetching, data, fetchNextPage, hasNextPage } =
     useInfinityQuery(debouncedInputText);
 
   useEffect(() => {
-    setIsSearchLoading(isFetching);
-  }, [isFetching]);
+    setIsSearchLoading(isLoading);
+  }, [isLoading]);
 
   useIntersectionObserver(ref, { threshold: 0.5 }, fetchNextPage);
 
-  if (inputText.length <= 0 || isFetching) return <></>;
+  if (inputText.length <= 0 || isLoading) return <></>;
   if (!data.length)
     return (
       <S.Container>
--- src/hooks/useInfinityQuery.ts
+++ src/hooks/useInfinityQuery.ts
@@ -3,28 +3,28 @@ import { useEffect, useState } from 'react';
 
 import { getSearch } from '../api/search';
 
-type ReturnType = [
-  isFetching: boolean,
-  data: string[],
-  fetchNextPage: () => Promise<void>,
-  hasNextPage: boolean
-];
+type ReturnType = {
+  isLoading: boolean;
+  isFetching: boolean;
+  data: string[];
+  fetchNextPage: () => Promise<void>;
+  hasNextPage: boolean;
+};
 
 export const useInfinityQuery = (keyword: string): ReturnType => {
+  const [isLoading, setIsLoading] = useState(false);
   const [isFetching, setIsFetching] = useState(false);
   const [data, setData] = useState<string[]>([]);
   const [hasNextPage, setHasNextPage] = useState(true);
   const [page, setPage] = useState(1);
 
   const fetchSearch = async (page: number) => {
-    setIsFetching(true);
     const response = await getSearch({ q: keyword, page });
 
     setHasNextPage(
       data.length + response.data.result.length !== response.data.total
     );
     setData((prev) => [...prev, ...response.data.result]);
-    setIsFetching(false);
     setPage(page + 1);
   };
 
@@ -32,13 +32,17 @@ export const useInfinityQuery = (keyword: string): ReturnType => {
     setData([]);
     if (!keyword) return;
 
+    setIsLoading(true);
     await fetchSearch(1);
+    setIsLoading(false);
   };
 
   const loadMore = async () => {
     if (!hasNextPage) return;
 
+    setIsFetching(true);
     await fetchSearch(page);
+    setIsFetching(false);
   };
 
   const fetchNextPage = loadMore;
@@ -47,5 +51,5 @@ export const useInfinityQuery = (keyword: string): ReturnType => {
     load();
   }, [keyword]);
 
-  return [isFetching, data, fetchNextPage, hasNextPage];
+  return { isLoading, isFetching, data, fetchNextPage, hasNextPage };
 };
