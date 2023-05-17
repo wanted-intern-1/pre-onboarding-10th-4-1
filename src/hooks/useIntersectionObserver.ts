d51f9144148f2691ab9a1a3fceff57060c08d862
정승덕 <akk9132@naver.com>
2023-05-17 12:04:30 +0900

    refator: Add useThrottle hook

--- src/hooks/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver.ts
@@ -1,15 +1,18 @@
 import { RefObject, useEffect } from "react";
+import { useThrottle } from "./useThrottle";
 
 export const useIntersectionObserver = (
   ref: RefObject<Element>,
   options: IntersectionObserverInit,
   callback: () => void
 ): void => {
+  const onThrottledCallback = useThrottle(callback, 300);
+
   useEffect(() => {
     const observer = new IntersectionObserver(async ([entry], observer) => {
       if (entry.isIntersecting) {
         observer.unobserve(entry.target);
-        callback();
+        onThrottledCallback();
         observer.observe(entry.target);
       }
     }, options);
@@ -18,5 +21,5 @@ export const useIntersectionObserver = (
 
     observer.observe(ref.current);
     return () => observer.disconnect();
-  }, [callback, options, ref]);
+  }, [onThrottledCallback, options, ref]);
 };
